7049706a37f166b980a438838fd2457410f7e21c
Mari Maisuradze <mari.maisura@gmail.com>
2022-11-01 13:34:36 +0200

    Add user model class

---
+++ DataLayer/Models/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+        public User(string email, string password, string? firstName, string? lastName)
+        {
+            Email = email;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string? FirstName { get; set; } = string.Empty;
+        [MaxLength(50)]
+        public string? LastName { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(50)]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public virtual List<Resolution> Resolutions { get; set; }
+
+    }
+}
