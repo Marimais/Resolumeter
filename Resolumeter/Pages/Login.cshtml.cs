5a7168dd107fa3c7633c0be6bb4d56b0d1ae6693
Mari Maisuradze <mari.maisura@gmail.com>
2022-11-02 15:33:19 +0200

    Add login page

--- Resolumeter/Pages/Login.cshtml.cs
+++ Resolumeter/Pages/Login.cshtml.cs
@@ -1,12 +1,22 @@
+using DataLayer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Resolumeter.Services;
 
 namespace Resolumeter.Pages
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public new User User { get; set; } = default!;
+
         public void OnGet()
         {
+            
+            var user=UserService.GetUser(User);
+
+
         }
+
     }
 }
