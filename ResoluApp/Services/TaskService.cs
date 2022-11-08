0c1907838ab41120bb2899144d2516aafcf70bc4
Mari <mari.maisura@gmail.com>
2022-11-08 13:20:13 +0200

    Add log in page

---
+++ ResoluApp/Services/TaskService.cs
@@ -0,0 +1,20 @@
+﻿using DataLayer.Models;
+using DataLayer.DataAccess;
+using Task = DataLayer.Models.Task;
+
+namespace ResoluApp.Services
+{
+    public static class TaskService
+    {
+        private static readonly ResolutionContext _context=new();
+
+        public static List<Task> GetAll()
+        {
+            _context.Database.EnsureCreated();
+            return _context.Tasks!.ToList();
+        }
+
+        
+
+    }
+}
