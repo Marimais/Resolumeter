93c0b20d9e17dba63a314c437bc248a7b1d98fd9
Mari <mari.maisura@gmail.com>
2022-11-24 14:03:50 +0200

    Add Index for Resolution

---
+++ DataLayer/Migrations/20221124093048_addIndexes.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class addIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Goals_GoalId",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GoalId",
+                table: "Tasks",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Resolutions",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resolutions_UserName_Year",
+                table: "Resolutions",
+                columns: new[] { "UserName", "Year" },
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Goals_GoalId",
+                table: "Tasks",
+                column: "GoalId",
+                principalTable: "Goals",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Goals_GoalId",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Resolutions_UserName_Year",
+                table: "Resolutions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GoalId",
+                table: "Tasks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Resolutions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Goals_GoalId",
+                table: "Tasks",
+                column: "GoalId",
+                principalTable: "Goals",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
