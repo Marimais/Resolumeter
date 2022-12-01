66806fe5a98537a58e2c973108ed9134371b94da
Mari <mari.maisura@gmail.com>
2022-12-01 17:14:09 +0200

    Add goal edit page

--- ResoluApp/Services/IGoalService.cs
+++ ResoluApp/Services/IGoalService.cs
@@ -5,6 +5,7 @@ namespace ResoluApp.Services
     internal interface IGoalService
     {
         void Create(string name, string? description, DateTime endDate);
+        public void Edit(int goalId, String name, String? description, DateTime endDate);
         void Delete(Goal goal);
         Goal? Get(int id);
         Goal? Get(string name);
